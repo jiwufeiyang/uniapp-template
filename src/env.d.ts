a6c1497d65a734ed0bf8ecd255fd8694a1eb3f31
jiwufeiyang <1257270453@qq.com>
2022-09-06 00:33:30 +0800

    fix: 环境变量配置

--- src/env.d.ts
+++ src/env.d.ts
@@ -5,3 +5,8 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>
   export default component
 }
+/** 扩展环境变量import.meta.env */
+interface ImportMetaEnv {
+  /** 这里增加自定义的声明 */
+  VITE_REQUEST_BASE_URL: string
+}
